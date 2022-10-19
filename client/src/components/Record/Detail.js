0622cb5e80395d75a6e4be948f693352fa6dff63
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2022-10-19 14:50:20 +0900

    feat: PDF 다운받기(서식 수정필요)
    보완해야할 사항: 글씨가 잘려서 pdf 서식 수정을 해야 할것 같아요. 추후에 일단 수정하겠습니다.
    + moment 패키지 설치했습니다.

--- client/src/components/Record/Detail.js
+++ client/src/components/Record/Detail.js
@@ -1,67 +1,91 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios'
 import server from './../../config/server.json'
-import {Button} from '@mui/material';
+import { Button } from '@mui/material';
 import { useCookies } from "react-cookie";
 import { useLocation } from 'react-router-dom';
+import { jsPDF } from "jspdf";
+import _fonts from "../../assets/fonts/gulim";
+import moment from "moment";
 
 const Detail = () => {
-    // console.log(useLocation().state) // list에서 받아 온 detail data 
+  // console.log(useLocation().state) // list에서 받아 온 detail data 
 
-    const [cookies, setCookie, removeCookie] = useCookies(["token"]);
-    
-    const [noteData, setNoteData] = useState(useLocation().state);
+  const [cookies, setCookie, removeCookie] = useCookies(["token"]);
 
-    const [newData, setNewData] = useState({
-      user_id : cookies.token.user_id,
-      title : "수정1",
-      contents : "수정했습니다",
-      file_url : "",
-      memo : "수정한 메모에요",
-      favorites: "",
+  const [noteData, setNoteData] = useState(useLocation().state);
+
+  const [newData, setNewData] = useState({
+    user_id: cookies.token.user_id,
+    title: "수정1",
+    contents: "수정했습니다",
+    file_url: "",
+    memo: "수정한 메모에요",
+    favorites: "",
   });
 
-    useEffect(() => {
-        console.log();
-        // getNoteData()
+  useEffect(() => {
+    console.log();
+    // getNoteData()
 
-        // updateNoteData()
-        // .then((res) => {
-        //   console.log('응답코드입니다', res);
-        //   setNoteData(res.data.user);
-        // })
-        // .catch((err) => {
-        //   console.log(err);
-        // });
-    }, []);
+    // updateNoteData()
+    // .then((res) => {
+    //   console.log('응답코드입니다', res);
+    //   setNoteData(res.data.user);
+    // })
+    // .catch((err) => {
+    //   console.log(err);
+    // });
+  }, []);
 
 
-    //노트 데이터를 요청하는 함수
-    let getNoteData = async () => {
-        return await axios.post("http://localhost:8080/record", noteData , {
-          header: {
-            accessToken: cookies.token.accessToken,
-          },
-        });
-    };
+  //노트 데이터를 요청하는 함수
+  let getNoteData = async () => {
+    return await axios.post("http://localhost:8080/record", noteData, {
+      header: {
+        accessToken: cookies.token.accessToken,
+      },
+    });
+  };
 
-    let updateNoteData = async () => {
-      return await // http://localhost:8080/record/user_id/update
+  let updateNoteData = async () => {
+    return await // http://localhost:8080/record/user_id/update
       axios.post(`http://localhost:8080/record/${newData.user_id}/update`,
-      newData, {
+        newData, {
         headers: {
           accessToken: cookies.token.accessToken
         }
 
       })
-    }
+  }
 
-    return (
-        <div>
-          <input type="text" name="title" defaultValue={noteData.title} />
-          <textarea name="contents" defaultValue={noteData.contents} />
-        </div>
-    );
+  let downloadBtn = (noteData) => {
+    var doc = new jsPDF("p", "mm", "a4");
+
+    doc.addFileToVFS('malgun.ttf', _fonts.font);  //_fonts 변수는 Base64 형태로 변환된 내용입니다.
+    doc.addFont('malgun.ttf', 'malgun', 'normal');
+    doc.setFont('malgun');
+
+    doc.line(10, 19, 200, 19); // 선그리기(시작x, 시작y, 종료x, 종료y)
+    doc.text(10, 15, noteData.title); // 글씨입력(시작x, 시작y, 내용)
+    doc.text(10, 30, noteData.contents); // 글씨입력(시작x, 시작y, 내용)
+
+    doc.save(`${moment(noteData.createdAt).format("YYYY-MM-DD")}_${noteData.title}.pdf`);  //결과 출력
+  }
+
+  console.log("노트데이터", noteData);
+
+  return (
+    <div>
+      <input type="text" name="title" defaultValue={noteData.title} />
+      <p>생성 날짜: {moment(noteData.createdAt).format("YYYY-MM-DD HH:mm:ss")}</p>
+      <p>수정 날짜: {moment(noteData.updatedAt).format("YYYY-MM-DD HH:mm:ss")}</p>
+      <textarea name="contents" defaultValue={noteData.contents} />
+      <button name="download" onClick={() => {
+        downloadBtn(noteData);
+      }}>PDF 다운로드</button>
+    </div>
+  );
 };
 
 export default Detail;
