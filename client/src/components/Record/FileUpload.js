cea15fa70edebc907aa1a5b65af41c45dc246204
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2022-10-20 18:24:13 +0900

    feat: 실시간 녹음 기능 추가

--- client/src/components/Record/FileUpload.js
+++ client/src/components/Record/FileUpload.js
@@ -1,13 +1,16 @@
-import { Box, Grid, Button, IconButton, TextField, Typography, Link } from '@mui/material'
+import { Box, Grid, Button, IconButton, TextField, Typography, Link } from '@mui/material';
+
 import FileUploadIcon from '@mui/icons-material/FileUpload';
 import { useNavigate } from 'react-router-dom';
+import AudioRecord from "./AudioRecord";
+import { useState, useEffect } from 'react';
 
 const FileUpload = () => {
     const navigate = useNavigate();
 
     const handleChangeFile = (e) => {
         navigate('/record/create', { state: e.target.files[0] });
-    } 
+    }
 
     const PtagStyle = {
         textAlign: 'center',
@@ -17,19 +20,20 @@ const FileUpload = () => {
     }
 
     return (
-        <Box sx={{ 
+        <Box sx={{
             position: 'absolute',
             top: '50%',
             left: '60%',
-            width:'60%',
+            width: '60%',
             transform: 'translate(-50%, -50%)'
-          }}>
-            <p style={PtagStyle}>내 PC에서 음성 파일을 첨부하세요. </p>
-            <p style={PtagStyle}>최대 250MB의 mp4, m4a, mp3, amr, flac, wav 확장자를 지원합니다.</p>
-            <div style={{textAlign:'center'}}>
-                <IconButton variant="contained" aria-label="upload file" component="label" size="large" style={{background: '#ddd'}}>
-                    <input type="file" id="file" hidden onChange={handleChangeFile}/>
-                    <FileUploadIcon fontSize="inherit"/>
+        }}>
+            <p style={PtagStyle}>실시간 녹음을 하거나 내 PC에서 음성 파일을 첨부하세요. </p>
+            <p style={PtagStyle}>녹음은 최대 1시간까지 가능합니다.<br/>최대 250MB의 mp4, m4a, mp3, amr, flac, wav 확장자를 지원합니다.</p>
+            <div style={{ textAlign: 'center' }}>
+                <AudioRecord />
+                <IconButton variant="contained" aria-label="upload file" component="label" size="large" style={{ background: '#ddd' }}>
+                    <input type="file" id="file" hidden onChange={handleChangeFile} />
+                    <FileUploadIcon fontSize="inherit" />
                 </IconButton>
             </div>
         </Box>
