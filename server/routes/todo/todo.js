697da28ac98947c47e33be49b82f5d89344c5547
yoonhee Lee <dasjuni@gmail.com>
2022-10-20 16:50:15 +0900

    feat: todo 백 url 수정했습니다.

--- server/routes/todo/todo.js
+++ server/routes/todo/todo.js
@@ -9,7 +9,7 @@ moment.tz.setDefault("Asia/Seoul");
 //localhost:8080/todo/write 투두 작성
 router.post("/write", async (req, res, next) => {
   console.log(req.body);
-  let { content , user_id, date } = req.body;
+  let { content, user_id, date } = req.body;
 
   let checkEmail = await Todo.findOne({ user_id });
 
@@ -40,10 +40,10 @@ router.post("/write", async (req, res, next) => {
 });
 
 //localhost:8080/todo/ 투두 불러오기
-router.get("/", async (req, res, next) => {
-  console.log(req.body);
+router.get("/:user_id", async (req, res, next) => {
+  console.log(req.params);
 
-  let { user_id } = req.body;
+  let { user_id } = req.params;
 
   let checkEmail = await Todo.findOne({ user_id });
   // db.articles.find( { “writer”: “Velopert”, “likes”: { $lt: 10 } } )
@@ -70,23 +70,21 @@ router.get("/", async (req, res, next) => {
 router.delete("/remove/:id", async (req, res, next) => {
   console.log(req.param);
 
-  const {id} = req.params;
+  const { id } = req.params;
 
   try {
-
     await Todo.findByIdAndRemove(id).exec();
 
-      console.log("\u001b[31m", "==== Success!! Remove TodoTask ====");
-      console.log("id: " + id);
-    
-      res.json({
-        message : "삭제 성공"
-      });
+    console.log("\u001b[31m", "==== Success!! Remove TodoTask ====");
+    console.log("id: " + id);
+
+    res.json({
+      message: "삭제 성공",
+    });
   } catch (err) {
     console.log("\u001b[31m", "==== Fail!! Remove TodoTask ====");
     console.error(err);
   }
-
 });
 
 module.exports = router;
